51e98770173fac4c277e1c9854165e3258974305
SundharAK <sundhar.aravazhi@gmail.com>
2026-02-16 16:07:07 +0000

    modified the app files

--- app/main.py
+++ app/main.py
@@ -1,61 +1,32 @@
 from fastapi import FastAPI
 import mysql.connector
-import time
 
 app = FastAPI()
 
-def connect_db():
-    while True:
-        try:
-            connection = mysql.connector.connect(
-                host="mysql",
-                user="root",
-                password="root123",
-                database="testdb"
-            )
-            return connection
-        except:
-            print("Waiting for MySQL...")
-            time.sleep(2)
-
-# Create table automatically (runs when API starts)
-@app.on_event("startup")
-def create_table():
-    conn = connect_db()
-    cursor = conn.cursor()
-    cursor.execute("""
-        CREATE TABLE IF NOT EXISTS users (
-            id INT AUTO_INCREMENT PRIMARY KEY,
-            name VARCHAR(50)
-        )
-    """)
-    conn.commit()
+def get_connection():
+    return mysql.connector.connect(
+        host="mysql",        # important: service name from docker-compose
+        user="root",
+        password="root123",
+        database="testdb"
+    )
 
 @app.get("/")
 def read_root():
-    conn = connect_db()
-    cursor = conn.cursor()
-    cursor.execute("SELECT DATABASE();")
-    db_name = cursor.fetchone()
-    return {"Connected to database": db_name}
-
-# API to insert data
-@app.get("/add/{name}")
-def add_user(name: str):
-    conn = connect_db()
-    cursor = conn.cursor()
-    cursor.execute("INSERT INTO users (name) VALUES (%s)", (name,))
-    conn.commit()
-    return {"message": f"User {name} inserted successfully"}
+    return {"message": "API is running"}
 
 @app.get("/users")
 def get_users():
-    conn = connect_db()
-    cursor = conn.cursor()
+    conn = get_connection()
+    cursor = conn.cursor(dictionary=True)
+
     cursor.execute("SELECT * FROM users")
-    data = cursor.fetchall()
-    return {"users": data}
+    result = cursor.fetchall()
 
+    cursor.close()
+    conn.close()
 
-volumes:
-  mysql_data:
+    return result
+
+if __name__ == "__main__":
+     app.run(host='0.0.0.0', port=8000)
